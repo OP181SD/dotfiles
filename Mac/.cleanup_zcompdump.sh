224d921c2afe1f7c68a824c6799d88a5e7dbc619
Yassine Saidi <71220894+OP181SD@users.noreply.github.com>
2024-06-05 18:23:58 +0200

    Delete Mac/.cleanup_zcompdump.sh

--- Mac/.cleanup_zcompdump.sh
+++
@@ -1,8 +0,0 @@
-# Supprimer les fichiers .zcompdump lors de la fermeture de la session Zsh
-function cleanup_zcompdump() {
-    find "$HOME" -type f -name ".zcompdump*" -exec rm {} \;
-    find "$HOME" -type f -name ".zwc" -exec rm {} \;
-}
-
-# Enregistrez la fonction pour qu'elle s'exécute lors de la fermeture de la session
-trap 'cleanup_zcompdump' EXIT
